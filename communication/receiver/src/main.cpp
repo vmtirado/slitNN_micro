1ea83c1c40a3d411e093a103bbbb9b398a72a2cd
vm.tirado <vm.tirado@uniandes.edu.co>
2022-05-18 10:16:05 -0500

    receiver con float

--- communication/receiver/src/main.cpp
+++ communication/receiver/src/main.cpp
@@ -3,10 +3,11 @@
 #define TXp2 17
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
+  Serial.begin(9600);
+  Serial2.begin(115200, SERIAL_8N1, RXp2, TXp2);
 }
 void loop() {
     Serial.println("Message Received: ");
-    Serial.println(Serial2.readString());
+    String data=Serial2.readString();
+    Serial.println(data.toFloat(),8);
 }
