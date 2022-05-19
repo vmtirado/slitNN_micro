96d978a3a781e5e48341a9bb48c73ce592bdc790
vm.tirado <vm.tirado@uniandes.edu.co>
2022-05-19 13:01:03 -0500

    Ya envio y recibo un float

--- communication/receiver/src/main.cpp
+++ communication/receiver/src/main.cpp
@@ -1,13 +1,20 @@
 #include <Arduino.h>
 #define RXp2 16
 #define TXp2 17
+
+ float serial_data;
+ unsigned char serial_buffer[4];
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial2.begin(115200, SERIAL_8N1, RXp2, TXp2);
 }
-void loop() {
-    Serial.println("Message Received: ");
-    String data=Serial2.readString();
-    Serial.println(data.toFloat(),8);
+void loop()
+{
+ if (Serial2.readBytes(serial_buffer, sizeof(float)) == sizeof(float)){
+   Serial.print("Asigne");
+   memcpy(&serial_data, serial_buffer, sizeof(float));
+   Serial.println(serial_data,4);
+ }
+     
 }
--- communication/sender/src/main.cpp
+++ communication/sender/src/main.cpp
@@ -6,36 +6,27 @@ float f2=0.018006956204771996;
 float arr[2]={0.471124,0.018006};
 
 int flag=0;
+
 //int f=20;
+union float_split
+{
+    uint8_t     fBytes[sizeof( float )];
+    float       fValue;
+};
+float_split flt;
 void setup() {
   // put your setup code here, to run once:
+  flt.fValue=0.471124;
   Serial.begin(9600);
   Serial2.begin(115200, SERIAL_8N1, RXp2, TXp2);
 }
-void loop() {
 
-//Serial.printf("Entre al loop %d",flag);
-if(flag==0){
-  Serial.println("Empiezo a imprimir");
-  Serial2.println(sizeof(arr)/sizeof(arr[0]));
-  delay(500);
+void loop()
+{    
+  
+    Serial2.write( flt.fBytes, sizeof( float ) );
+    Serial.println(flt.fValue,4);
 
-  Serial.printf("Mande tamaño %d \n",sizeof(arr)/sizeof(arr[0]));
-  for (int i=0; i<sizeof(arr)/sizeof(arr[0]);i++){
-    Serial.println("Estoy en el for "+String(arr[i],6));
-    Serial2.println(String(arr[i],6));
-  delay(500);
-  }
-  //flag=-1;
-  Serial.println("Termine de imprimir");
+    delay(1000);
+    
 }
-  delay(500);
-
-
-
-  //Serial2.println(st);
-  //Serial.print(st.toFloat(),8);
-
-
-//Serial.printf("Flag es %d \n",flag); 
-}  
